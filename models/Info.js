0d4cd2e2657b329563f286ded4779a35b3ce3cd3
keshaji <klastertiga@gmail.com>
2026-05-17 20:53:49 +0700

    Add Info model

---
+++ models/Info.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const infoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Info', infoSchema);
